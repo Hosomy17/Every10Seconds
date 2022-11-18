45302e39976270d4c3679922989e456d136d6f7f
Daniela Raposo <danielas209@hotmail.com>
2022-11-18 18:08:33 -0400

    Cena creditos

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -5,10 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public void OnLevelWasLoaded(int i)
-    {
-        SceneManager.LoadScene(i);
-    }
+
 
     public void Load(string s)
     {
