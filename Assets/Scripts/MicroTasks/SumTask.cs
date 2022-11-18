a8494bbc59bb0d071956e83f3641424f7774ab05
Jhenifer <jheniferchrist.95@gmail.com>
2022-11-18 18:17:12 -0400

    fix/sumTask

--- Assets/Scripts/MicroTasks/SumTask.cs
+++ Assets/Scripts/MicroTasks/SumTask.cs
@@ -3,35 +3,19 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class SumTask : MicroTask
-{[SerializeField]
+{    [SerializeField]
     private string _correct;
     
     [SerializeField]
     private string _wrong;
     
-    [SerializeField]
-    private string _number1;
-    
-    [SerializeField]
-    private string _number2;
-    
     private int _awnser;
-    
-    [SerializeField]
-    private Image _image;
 
     [SerializeField]
     private TMPro.TMP_Text _leftOption;
     
     [SerializeField]
     private TMPro.TMP_Text _rightOption;
-    
-    [SerializeField]
-    private TMPro.TMP_Text _rightNumber;
-    
-    [SerializeField]
-    private TMPro.TMP_Text _leftNumber;
-
     public override void StartTask(Finished callback)
     {
         base.StartTask(callback);
@@ -44,17 +28,13 @@ public class SumTask : MicroTask
         {
             _leftOption.text = _correct;
             _rightOption.text = _wrong;
-            _leftNumber.text = _number1;
-            _rightOption.text = _number2;
-
         }
         else
         {
             _leftOption.text = _wrong;
             _rightOption.text = _correct;
-            _leftNumber.text = _number2;
-            _rightOption.text = _number1;
         }
+        
     }
 
     public void Update()
