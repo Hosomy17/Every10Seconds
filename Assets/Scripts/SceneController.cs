e711a7880f831cd929851b8dbb429797ddfb1729
Daniela Raposo <danielas209@hotmail.com>
2022-11-18 16:52:31 -0400

    adding a Menu Scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/*public class SceneController : MonoBehaviour
+{
+    //public Image blackfade;
+    public Animator animFade;
+    private string sceneToLoad;
+
+    public void Restart (string scene)
+    {
+        sceneToLoad = scene;
+        StartCoroutine(GoToNextLevel());
+    }
+    IEnumerator GoToNextLevel()
+    {
+        animFade.SetBool("fade", true);
+       // yield return new WaitUntil(() => blackfade.color.a == 1);
+        SceneManager.LoadScene(sceneToLoad);
+    }*/
+//}//
